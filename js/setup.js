e79ecddd11e37e34078fae662c57c26640b8186e
4el213@gmail.com <4el213@gmail.com>
2018-06-27 23:37:59 +0300

    исправил замечания

--- js/setup.js
+++ js/setup.js
@@ -1,75 +1,5 @@
 'use strict';
 
-//  utils.js
-(function () {
-  var KEY_CODE_ENTER = 13;
-  var KEY_CODE_ESC = 27;
-
-  window.utils = {
-    isEnterKeycode: function (evt) {
-      return evt.keyCode === KEY_CODE_ENTER;
-    },
-
-    isEscKeycode: function (evt) {
-      return evt.keyCode === KEY_CODE_ESC;
-    }
-  };
-})();
-
-
-//  create-wizards.js
-(function () {
-  var namesList = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
-  var familyList = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
-  var coatColorList = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
-  var eyesColorList = ['black', 'red', 'blue', 'yellow', 'green'];
-  var amountWizards = 4;
-
-  var selectRandomItem = function (itemsList) {
-    var i = Math.floor((Math.random() * itemsList.length));
-    return itemsList[i];
-  };
-
-  var createListFeatures = function () {
-    var listFeature = {
-      name: selectRandomItem(namesList) + ' ' + selectRandomItem(familyList),
-      coatColor: selectRandomItem(coatColorList),
-      eyesColor: selectRandomItem(eyesColorList)
-    };
-    return listFeature;
-  };
-
-  var createWizards = function (amtWizards) {
-    var wizards = [];
-    for (var i = 0; i < amtWizards; i++) {
-      wizards[i] = createListFeatures();
-    }
-    return wizards;
-  };
-
-  var wizards = createWizards(amountWizards);
-
-  var similarItem = document.querySelector('.setup-similar');
-  var similarListItem = document.querySelector('.setup-similar-list');
-  var similarListTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-
-  similarItem.classList.remove('hidden');
-
-  for (var i = 0; i < amountWizards; i++) {
-    var simularWizard = similarListTemplate.cloneNode(true);
-    simularWizard.querySelector('.setup-similar-label').textContent = wizards[i].name;
-    simularWizard.querySelector('.wizard-coat').style.fill = wizards[i].coatColor;
-    simularWizard.querySelector('.wizard-eyes').style.fill = wizards[i].eyesColor;
-    similarListItem.appendChild(simularWizard);
-  }
-
-  window.createWizards = {
-    coatColorList: coatColorList,
-    eyesColorList: eyesColorList
-  };
-})();
-
-//  render-setup.js
 (function () {
   var fireballColorList = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
   var setup = document.querySelector('.setup');
@@ -137,74 +67,9 @@
     });
   };
 
-  window.renderSetup = {
-    setup: setup,
+  window.setup = {
+    element: setup,
     openSetup: openSetup
   };
 
 })();
-
-
-//  setup-open.js
-(function () {
-  var setupOpen = document.querySelector('.setup-open');
-  var setupOpenIcon = document.querySelector('.setup-open-icon');
-
-  setupOpen.addEventListener('click', function () {
-    window.renderSetup.openSetup();
-  });
-  setupOpenIcon.addEventListener('keydown', function (evt) {
-    if (window.utils.isEnterKeycode(evt)) {
-      window.renderSetup.openSetup();
-    }
-  });
-})();
-
-
-// setup-move.js
-(function () {
-  var controlSetup = document.querySelector('.upload');
-  controlSetup.addEventListener('mousedown', function (evt) {
-    evt.preventDefault();
-    var dragged = false;
-
-    var startCoordinates = {
-      x: evt.clientX,
-      y: evt.clientY
-    };
-    var onMouseMove = function (moveEvt) {
-      moveEvt.preventDefault();
-      dragged = true;
-      var shift = {
-        x: startCoordinates.x - moveEvt.clientX,
-        y: startCoordinates.y - moveEvt.clientY
-      };
-
-      startCoordinates = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
-      window.renderSetup.setup.style.top = (window.renderSetup.offsetTop - shift.y) + 'px';
-      window.renderSetup.setup.style.left = (window.renderSetup.setup.offsetLeft - shift.x) + 'px';
-    };
-
-    var onMouseUp = function (upEvt) {
-      upEvt.preventDefault();
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('mouseup', onMouseUp);
-      if (dragged) {
-        var onClickPreventDefault = function (clickEvt) {
-          clickEvt.preventDefault();
-          controlSetup.removeEventListener('click', onClickPreventDefault);
-        };
-        controlSetup.addEventListener('click', onClickPreventDefault);
-      }
-    };
-
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp);
-  });
-})();
-
-
